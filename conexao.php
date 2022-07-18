3730122f9dd190aae55e2ed9859e193e658a1214
Eryc <eryc.enrique@etools.com.br>
2022-07-18 03:17:50 -0300

    movendo arquivo de local

--- conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('HOST', '127.0.0.1');
-define('USER', 'root');
-define('PASSWORD', '');
-define('DB', 'conag-banco');
-
-$conexao = mysqli_connect(HOST, USER, PASSWORD, DB) or die ('ERROR!');
