f018a0a7e239f43fc2ac3c53f6b41dbe52b4e6a7
Eryc <eryc.enrique@etools.com.br>
2022-07-19 05:09:08 -0300

    Update em ajax jquery

--- js/principal.js
+++ js/principal.js
@@ -16,3 +16,21 @@ $("#bt_prod").on("click",function(){
         $("#resposta").html("Prodto cadstrado com sucesso!");
     })
 })
+$("#bt_atualizar").on("click", function(){
+    var input_prod = $("#prod").val();
+    var input_valor = $("#valor").val();
+
+    $.ajax({
+        url: "../crud/update.php",
+        type: "post",
+        data:{
+            produto: input_prod,
+            valor: input_valor
+        },
+        beforeSend : function(){
+            $("#resposta").html("Enviando!");
+        }
+    }).done(function(r){
+        $("#resposta").html("Atualizado com sucesso!");
+    })
+})
