bfd491ba07a11f7d4fbd335edc22abd7ba08626b
Eryc <eryc.enrique@etools.com.br>
2022-07-19 03:04:54 -0300

    Metodo de criaçao de produto implemtentado

---
+++ js/principal.js
@@ -0,0 +1,18 @@
+$("#bt_prod").on("click",function(){
+    var input_prod = $("#prod").val();
+    var input_valor = $("#valor").val();
+
+    $.ajax({
+        url: "../crud/create.php",
+        type: "post",
+        data:{
+            produto: input_prod,
+            valor: input_valor
+        },
+        beforeSend : function(){
+            $("#resposta").html("Enviando!");
+        }
+    }).done(function(r){
+        $("#resposta").html("Prodto cadstrado com sucesso!");
+    })
+})
